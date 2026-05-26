4b79763b79c58186ef746a6ef93fc5022a420f9c
kakeicloud-APP <kakeicloud@e4.gmobb.jp>
2026-05-27 06:40:17 +0900

    Update version.tsv2.2.24

--- kakeicloud-app/lib/version.ts
+++ kakeicloud-app/lib/version.ts
@@ -1,7 +1,7 @@
 /**
- * kakeicloud  v2.2.23 | 2026/05/28
+ * kakeicloud  v2.2.24 | 2026/05/28
  * kakeicloud-app/lib/version.ts
  */
 
-export const VERSION = " v2.2.23"
+export const VERSION = " v2.2.24"
 
